ea54009ade47ad4a7a0ebf1a5614024344b39876
Elizaveta Biryukova <birliza@mail.ru>
2023-04-28 11:58:49 +0200

    feat: create class

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-const name = 'Liza';
+class Keyboard {
+  constructor() {
+    this.textarea = null;
+    this.container = null;
+  }
+}
+
+const keyboard = new Keyboard();
+
+function keyboardInit() {
+  keyboard.textarea = document.createElement('textarea');
+  keyboard.textarea.classList.add('text');
+  keyboard.container = document.createElement('div');
+  keyboard.container.classList.add('container');
+
+  document.body.append(keyboard.textarea, keyboard.container);
+}
+
+keyboardInit();
